81a52850a0a771afeda3a85a99f99d44575249dc
nitinnavik <naviknitin143@gmail.com>
2022-06-29 18:39:44 +0530

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Search from "./Components/search";
+import Image from "./Components/Image";
+import Pagination from "./Components/Pagination";
+import React from "react";
 
 function App() {
+  const [Data, setData] = useState("");
+  const [text ,setText] = useState("")
+
+  const [currentPage, setCurrentPage] = useState(1);
+  let totalPages = 5;
+
+  const FetchImage = async (data) => {
+    console.log(data)
+    let token = "gHd5zbIGRozbuDAY1SK7q2oVBSkiMA5ThID1ZqyWoTk";
+    let url = `https://api.unsplash.com/photos?page=2`;
+   
+    if (data) {
+      url = `https://api.unsplash.com/search/photos?query=${text}&page=${currentPage}`;
+    }
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Client-ID ${token}`,
+      },
+    });
+    setData(response.data);
+    console.log(response);
+    if (text) {
+      setData(response.data.results);
+    }
+  };
+  console.log(Data);
+
+  const handlePageChange =(page)=> {
+    setCurrentPage(page);
+    let data =({text:text,page:page})
+    FetchImage(data)
+  }
+
+  useEffect(() => {
+    FetchImage();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search setText={setText} FetchImage={FetchImage} text={text} />
+      {Data ? <Image item={Data} /> : ""}
+      <Pagination  currentPage={currentPage} handlePageChange={handlePageChange} totalPages={totalPages}/>
     </div>
   );
 }
